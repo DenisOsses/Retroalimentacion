6f29f043df71004dc60be11a8247387f59b9563b
Denis Osses <110506752+DenisOsses@users.noreply.github.com>
2025-11-11 08:16:06 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Retroalimentacion/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
